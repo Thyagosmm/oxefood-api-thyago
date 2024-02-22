c3bbf095c13f84c6da78e3aa257e4802c6c49858
aluno <atendimento.secretaria@jaboatao.ifpe.edu.br>
2024-02-22 15:41:35 -0300

    primeiras configurações

---
+++ src/main/java/br/com/ifpe/oxefoodapithyago/OxefoodApiThyagoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapithyago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiThyagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiThyagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefoodapithyago/OxefoodApiThyagoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodapithyago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApiThyagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
