47b64ba4e06e53bd31bd40c0445fa958736774a1
aluno <tsm6@discente.ifpe.edu.br>
2024-08-14 17:28:04 -0300

    alterando security configuration

--- src/main/java/br/com/ifpe/oxefoodapithyago/config/SecurityConfiguration.java
+++ src/main/java/br/com/ifpe/oxefoodapithyago/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,29 +25,32 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
+    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter,
+            AuthenticationProvider authenticationProvider) {
 
         this.authenticationProvider = authenticationProvider;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf()
-                .disable()
-                .authorizeHttpRequests()
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
 
-                .requestMatchers(HttpMethod.POST, "/api/cliente").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/produto").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/produto/*").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/entregador").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/categoriaproduto").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/produto").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/cliente").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/auth").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()
 
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                        .anyRequest().authenticated()
+
+                )
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
